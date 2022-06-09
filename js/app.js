dc1c68416558e1952e3f9b45370f584c4dba2b7c
Nayan Islam <nayannk103@gmail.com>
2022-06-09 22:16:55 +0600

    update

--- js/app.js
+++ js/app.js
@@ -1,27 +1,40 @@
 ////////////apis//////////
+const searchPeram = location.search.split('=').pop();
 const api_accesKey = `fTCd0sVXOLT0uQYq2b5vV_Bq8Eaw3y26LUq-PEf5lUM`;
-const api_Url = `https://api.unsplash.com/photos/random?client_id=${api_accesKey}&count=50`;
+const api_Url = `https://api.unsplash.com/photos?client_id=${api_accesKey}&count=50`;
+const search_api_Url = `https://api.unsplash.com/search/photos?client_id=${api_accesKey}&query=${searchPeram}&per_page=50`;
+
 //////elements///////
 const warper = document.querySelector('.warper');
 let allData;
-const getImages = () => {
-    fetch(`${api_Url}`)
-    .then(response => response.json())
+let currentImg = 0;
+const getImages = async  () => {
+   await fetch(api_Url)
+    .then((response) => response.json())
     .then((data) => {
         allData = data;
-        createImg(allData)
+        createImg(allData);
+
     })
 }
-const createImg = (data) => {
+const searchImages = async  () => {
+  await fetch(search_api_Url)
+   .then((response) => response.json())
+   .then((allsData) => {
+       allData = allsData.results;
+       createImg(allData);
+   })
+}
+  ////////create imgs /////////
+  const createImg =async (data) => {
     let imgs = ` `;
-    
-    data.forEach((item) => {
+  await data.forEach((item, index) => {
         imgs += `
        <div class="item">
        <div class="item_top">
               <div class="left"><p></p></div>
               <div class="rigth">
-                <div class="love"><i class="fa-solid fa-heart"></i></div>
+                <div class="love" id="galleryLove"><i class="fa-solid fa-heart"></i></div>
                 <div class="plus"><i class="fa-solid fa-plus"></i></div>
               </div>
             </div>
@@ -29,6 +42,7 @@ const createImg = (data) => {
               <div class="item_bottom_left">
                 <div class="img">
                   <img
+                  class='itemImg'
                     src="./img/profile-1653477423424-2860ccb143aaimage.avif"
                     alt=""
                   />
@@ -54,16 +68,93 @@ const createImg = (data) => {
               </div>
               <div class="rigth">
                 <div class="download">
-                  <i class="fa-solid fa-arrow-down"></i>
+                <a href="${item.links.download}" download target="_blank">
+                  <i class="fa-solid fa-arrow-down" id="download"></i>
+                </a>
                 </div>
               </div>
             </div>
             <img class="img" src="${item.urls.regular}" alt="" />
           </div>
        `
-    })
-    warper.innerHTML = imgs;
-}
+       
 
-getImages()
-getImages()
+       warper.innerHTML = imgs;
+
+      const images =document.querySelectorAll('.img');
+      images.forEach((img, index) => {
+        img.addEventListener('click', () => {
+          popUp.classList.add("show");
+          popUp.classList.remove("hide");
+
+          currentImg = index;
+          /////popup img/////
+          popUpImg(img);
+          
+          ////popUP Download////
+          popUpDownload(currentImg);
+
+          ///////slide img/////
+          slideImg(currentImg);
+        })
+      })
+    })
+
+    
+    //////add loves////////
+    const loves =document.querySelectorAll('#galleryLove');
+    loves.forEach((love) => {
+      love.addEventListener('click', () => {
+        love.classList.toggle("active")
+      })
+    })
+  }
+  /////////pop up imgs/////////
+  let popupImg = document.querySelector('#popupImg');
+  const popUpImg = (data)=>{
+    popupImg.src = `${data.src}`;
+    }
+    ////////popUP Download/////////
+  let popUpDownloadImg = document.querySelector('#popUpDownload');
+  let smallDownload = document.querySelector('#smallDownload');
+  let mediumDownload = document.querySelector('#mediumDownload');
+  let largeDownload = document.querySelector('#largeDownload');
+  let originalDownload = document.querySelector('#originalDownload');
+let popUpDownload = (currentImg) => {
+  popUpDownloadImg.href = `${allData[currentImg].links.download}`;
+  smallDownload.href = `${allData[currentImg].urls.regular}`;
+  mediumDownload.href = `${allData[currentImg].urls.regular}`;
+  largeDownload.href = `${allData[currentImg].urls.regular}`;
+  originalDownload.href = `${allData[currentImg].urls.regular}`;
+}
+  /////////image slide/////////
+  const prv = document.querySelector('#prv');
+  const next = document.querySelector('#next');
+  /////////prv/////////
+  let slideImg = (currentImg) => {
+    prv.addEventListener('click', () => {
+      if(currentImg > 0){
+        currentImg--;
+        let count = allData[currentImg - 1].urls.regular;
+        popupImg.src = `${count}`;
+      }
+    })
+    next.addEventListener('click', () => {
+      if(currentImg < allData.length){
+        currentImg++;
+       let count = allData[currentImg - 1].urls.regular;
+       popupImg.src = `${count}`;
+      }
+    })
+  }
+ 
+  
+
+  //////update car text//////////
+const carText = document.querySelector('.titleSearch');
+  if(searchPeram == ''){
+  getImages()
+  } else{
+    searchImages()
+carText.innerHTML = searchPeram;
+  }
--- js/download.js
+++ js/download.js
@@ -1,6 +1,15 @@
+const popUp =document.querySelector('.pop_up');
  const downloadIcon = document.querySelector('.download_icon');
  const downloadPopUP = document.querySelector('.download_popUp');
-
- downloadIcon.addEventListener('click', () => {
-    downloadPopUP.classList.toggle('active');
- })
+ const popUPlove =document.querySelector('.fevList');
+ const close_popUp =document.querySelector('.close_popUp');
+ const classAdd = (element) => {
+  element.classList.toggle('active')
+ }
+    downloadIcon.addEventListener('click', () => classAdd(downloadPopUP));
+    popUPlove.addEventListener('click', () => classAdd(popUPlove));
+    close_popUp.addEventListener('click', () =>{
+      popUp.classList.add('hide')
+      popUp.classList.remove('show')
+    })
+    
